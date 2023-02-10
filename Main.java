d5b53980477d0e490384029bb7ffd0c992bd7c87
Sanjana P <sanjanakalathil@gmail.com>
2023-02-10 16:06:25 +0530

    made some changes

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 
 class Browser{
 	private static String[] array;
-//	static String[] arraynew;
 			
 	Browser(){
 		
@@ -11,15 +10,8 @@ class Browser{
 	
 	Browser(String[] arr){
 		this.array = arr;
-//		this.arraynew = arr;
 	}
 	
-//	void test() {
-//		for (int i=0;i<4;i++)
-//		{
-//			System.out.println(a[i]);
-//		}
-//	}
 	
 	void setArray(String[] arr) {
 		this.array = arr;
@@ -37,45 +29,34 @@ class Main{
 
 	public static void main(String[] args) {
 		String[] arr = new String[4];
-//		String[] arrnew = new String[4];
 		String[] getmozilla = new String[4];
-		String[] getchrome = new String[4];
+//		String[] getchrome = new String[4];
 		
 		for (int i=0; i<4; i++)
 		{
 			Scanner s = new Scanner(System.in);
 			arr[i] = s.nextLine();
 		}
-//		for (int i=0; i<4; i++)
-//		{
-//			Scanner s = new Scanner(System.in);
-//	
-//		}
 		
 		
 		Browser chrome = new Browser();
 		Browser mozilla = new Browser(arr);
-//		Browser chrome = new Browser(arrnew);
-
-//		mozilla.test();
+		
+		
+		
 		
 		mozilla.setArray(arr);
 		getmozilla = mozilla.getArray();
 		
-//		chrome.setArray(arr);
-//		getchrome = chrome.getArray();
+
+//		for (int i=0; i<4; i++) {
+//					System.out.println(arr[i]);
+//		}
 		
 		for (int i=0; i<4; i++) {
 			System.out.println(getmozilla[i]);
 		}
-//		for (int i=0; i<4; i++) {
-//			System.out.println(getchrome[i]);
-//		}
-//		
-//		for(int i = 0; i<4; i++)
-//		{
-//			System.out.println(Browser.arraynew);
-//		}
+
 	
 		
  	}
